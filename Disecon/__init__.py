4f358003829b2bfa559f6ec466cdac2270618a0b
Mason <masonnorberg@midco.net>
2021-09-26 16:16:38 -0500

    added code to view and taking the view class from __init__ to view

--- Disecon/__init__.py
+++ Disecon/__init__.py
@@ -1,5 +1,6 @@
 import inspect
 import sqlite3
+from view import *
 from inspect import getframeinfo
 from inspect import stack
 
@@ -93,94 +94,7 @@ class wallet:
             for item in items:
                 pass
         
-class view:
-    def __init__(self, user_ID=None, user_Name=None):
-        self.user_ID = user_ID
-        self.user_Name = user_Name
-        
-    def wallet(self):
-        #both don't work
-        if self.user_ID == None and self.user_Name == None:
-            print(f"Nothing is in the view function {debuginfo()}")
-        
-        #user name works but user ID doesn't work
-        elif self.user_ID == None:
-            print("user_ID")
-        
-        #user ID works but user Name doesn't    
-        elif self.user_Name == None:            
-            conn = sqlite3.connect("economy.db")
-            c = conn.cursor()
-            
-            c.execute(f"SELECT * FROM economy WHERE user_ID={self.user_ID}")
-            
-            items = c.fetchall()
-            none = str(items)
-            
-            if none == "[]":
-                return "0"
-            
-            else:
-                for item in items:
-                    wallet = item[2]
-                    
-                return wallet        
 
-    def bank(self):
-        #both don't work
-        if self.user_ID == None and self.user_Name == None:
-            print(f"Nothing is in the view function {debuginfo()}")
-        
-        #user name works but user ID doesn't work
-        elif self.user_ID == None:
-            print("user_ID")
-        
-        #user ID works but user Name doesn't    
-        elif self.user_Name == None:            
-            conn = sqlite3.connect("economy.db")
-            c = conn.cursor()
-            
-            c.execute(f"SELECT * FROM economy WHERE user_ID={self.user_ID}")
-            
-            items = c.fetchall()
-            none = str(items)
-            
-            if none == "[]":
-                return "0"
-            
-            else:
-                for item in items:
-                    bank = item[3]
-                    
-                return bank
-        
-    def net(self):
-        #both don't work
-        if self.user_ID == None and self.user_Name == None:
-            print(f"Nothing is in the view function {debuginfo()}")
-        
-        #user name works but user ID doesn't work
-        elif self.user_ID == None:
-            print("user_ID")
-        
-        #user ID works but user Name doesn't    
-        elif self.user_Name == None:            
-            conn = sqlite3.connect("economy.db")
-            c = conn.cursor()
-            
-            c.execute(f"SELECT * FROM economy WHERE user_ID={self.user_ID}")
-            
-            items = c.fetchall()
-            none = str(items)
-            
-            if none == "[]":
-                return "0"
-            
-            else:
-                for item in items:
-                    net = item[4]
-                    
-                return net
             
 class top:
     def __init__(self, place:int):
@@ -202,7 +116,8 @@ something = wallet(100, 638092957756555291, f"LegosAndStuff#0501")
 
 #something.add()
 
-sun = view(user_ID=638092957756555291)
+#sun = view(user_ID=638092957756555291)
+sun = view()
 print(sun.wallet())
 print(sun.bank())
 print(sun.net())
---
+++ Disecon/view.py
@@ -0,0 +1,98 @@
+import inspect
+import sqlite3
+from inspect import getframeinfo
+from inspect import stack
+
+#def debuginfo():
+    #caller = getframeinfo(stack()[1][0])
+    
+    #return f"\n   File - {caller.filename} \n      Line - {caller.lineno}"
+
+class view:
+    def __init__(self, user_ID=None, user_Name=None):
+        self.user_ID = user_ID
+        self.user_Name = user_Name
+        
+    def wallet(self):
+        #both don't work
+        if self.user_ID == None and self.user_Name == None:
+            print(f"Nothing is in the view function")
+        
+        #user name works but user ID doesn't work
+        elif self.user_ID == None:
+            print("user_ID")
+        
+        #user ID works but user Name doesn't    
+        elif self.user_Name == None:            
+            conn = sqlite3.connect("economy.db")
+            c = conn.cursor()
+            
+            c.execute(f"SELECT * FROM economy WHERE user_ID={self.user_ID}")
+            
+            items = c.fetchall()
+            none = str(items)
+            
+            if none == "[]":
+                return "0"
+            
+            else:
+                for item in items:
+                    wallet = item[2]
+                    
+                return wallet        
+
+    def bank(self):
+        #both don't work
+        if self.user_ID == None and self.user_Name == None:
+            print(f"Nothing is in the view function {debuginfo()}")
+        
+        #user name works but user ID doesn't work
+        elif self.user_ID == None:
+            print("user_ID")
+        
+        #user ID works but user Name doesn't    
+        elif self.user_Name == None:            
+            conn = sqlite3.connect("economy.db")
+            c = conn.cursor()
+            
+            c.execute(f"SELECT * FROM economy WHERE user_ID={self.user_ID}")
+            
+            items = c.fetchall()
+            none = str(items)
+            
+            if none == "[]":
+                return "0"
+            
+            else:
+                for item in items:
+                    bank = item[3]
+                    
+                return bank
+        
+    def net(self):
+        #both don't work
+        if self.user_ID == None and self.user_Name == None:
+            print(f"Nothing is in the view function {debuginfo()}")
+        
+        #user name works but user ID doesn't work
+        elif self.user_ID == None:
+            print("user_ID")
+        
+        #user ID works but user Name doesn't    
+        elif self.user_Name == None:            
+            conn = sqlite3.connect("economy.db")
+            c = conn.cursor()
+            
+            c.execute(f"SELECT * FROM economy WHERE user_ID={self.user_ID}")
+            
+            items = c.fetchall()
+            none = str(items)
+            
+            if none == "[]":
+                return "0"
+            
+            else:
+                for item in items:
+                    net = item[4]
+                    
+                return net
