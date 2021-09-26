d641dcdd630331cc4e84f5d7e1f440893feb608e
Mason <masonnorberg@midco.net>
2021-09-26 11:44:51 -0500

    the view class is all working

--- Disecon/__init__.py
+++ Disecon/__init__.py
@@ -6,7 +6,7 @@ from inspect import stack
 def debuginfo():
     caller = getframeinfo(stack()[1][0])
     
-    return f"\nFile - {caller.filename} \nLine - {caller.lineno}"
+    return f"\n   File - {caller.filename} \n      Line - {caller.lineno}"
 
 def start():
     conn = sqlite3.connect("economy.db")
@@ -49,12 +49,14 @@ class wallet:
         none = str(items)
         
         if none == "[]":
-            c.execute(f"INSERT INTO economy VALUES ({self.user_ID}, '{self.user_Name}', {self.amount} , 0, 0)")
+            c.execute(f"INSERT INTO economy VALUES ({self.user_ID}, '{self.user_Name}', {self.amount} , 0, {self.amount})")
             print("made") #test part#
             
         else:
             for item in items:
                 wallet = int(item[2])
+                bank = int(item[3])
+
                 
             sum = self.amount + wallet
             
@@ -63,6 +65,15 @@ class wallet:
                 """)
             
             conn.commit()
+            
+            net = sum + bank
+            
+            c.execute(f"""UPDATE economy SET net = {net}
+                      WHERE user_ID = {self.user_ID}
+                """)
+            
+            conn.commit()
+            conn.close()
         
     def sub(self):
         wallet.amount = wallet.amount - 100
@@ -83,13 +94,82 @@ class view:
             print("user_ID")
         
         #user ID works but user Name doesn't    
-        elif self.user_Name == None:
-            print("user_name") 
+        elif self.user_Name == None:            
+            conn = sqlite3.connect("economy.db")
+            c = conn.cursor()
+            
+            c.execute(f"SELECT * FROM economy WHERE user_ID={self.user_ID}")
+            
+            items = c.fetchall()
+            none = str(items)
+            
+            if none == "[]":
+                return "0"
+            
+            else:
+                for item in items:
+                    wallet = item[2]
+                    
+                return wallet        
 
     def bank(self):
-        pass
+        #both don't work
+        if self.user_ID == None and self.user_Name == None:
+            print(f"Nothing is in the view function {debuginfo()}")
+        
+        #user name works but user ID doesn't work
+        elif self.user_ID == None:
+            print("user_ID")
+        
+        #user ID works but user Name doesn't    
+        elif self.user_Name == None:            
+            conn = sqlite3.connect("economy.db")
+            c = conn.cursor()
+            
+            c.execute(f"SELECT * FROM economy WHERE user_ID={self.user_ID}")
+            
+            items = c.fetchall()
+            none = str(items)
+            
+            if none == "[]":
+                return "0"
+            
+            else:
+                for item in items:
+                    bank = item[3]
+                    
+                return bank
         
     def net(self):
+        #both don't work
+        if self.user_ID == None and self.user_Name == None:
+            print(f"Nothing is in the view function {debuginfo()}")
+        
+        #user name works but user ID doesn't work
+        elif self.user_ID == None:
+            print("user_ID")
+        
+        #user ID works but user Name doesn't    
+        elif self.user_Name == None:            
+            conn = sqlite3.connect("economy.db")
+            c = conn.cursor()
+            
+            c.execute(f"SELECT * FROM economy WHERE user_ID={self.user_ID}")
+            
+            items = c.fetchall()
+            none = str(items)
+            
+            if none == "[]":
+                return "0"
+            
+            else:
+                for item in items:
+                    net = item[4]
+                    
+                return net
+            
+class top:
+    def __init__(self, place:int):
         pass
         
 start()
@@ -103,9 +183,11 @@ conn.commit()
 conn.close()
         
         
-someting = wallet(10, 638092957756555291, f"LegosAndStuff#0501")
+something = wallet(100, 638092957756555291, f"LegosAndStuff#0501")
 
-print(someting.add())
+#something.add()
 
-sun = view(user_Name=638092957756555291)
+sun = view(user_ID=638092957756555291)
 print(sun.wallet())
+print(sun.bank())
+print(sun.net())
