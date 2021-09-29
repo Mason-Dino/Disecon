cd2a050e9a2827d9b17b320f5dd1b7ca52ea777d
Mason <masonnorberg@midco.net>
2021-09-28 19:06:36 -0500

    re added the try and except

--- Disecon/__init__.py
+++ Disecon/__init__.py
@@ -14,15 +14,20 @@ def start():
     conn = sqlite3.connect("economy.db")
     c = conn.cursor()
     
-
-    c.execute("""CREATE TABLE economy (
-            user_ID int,
-            user_name text,
-            wallet int,
-            bank int,
-            net int
-            
-    )""")
+    try:
+        c.execute("""CREATE TABLE economy (
+                user_ID int,
+                user_name text,
+                wallet int,
+                bank int,
+                net int
+                
+        )""")
+        
+        pass
+    
+    except:
+        print("table wrong")
         
     
     conn.commit()
