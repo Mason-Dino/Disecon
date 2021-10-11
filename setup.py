e8dbfbebbf0d57182b3e4ae24b6ce4f430cbe947
Mason <masonnorberg@midco.net>
2021-10-11 11:12:55 -0500

    changed version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ classifiers = [
  
 setup(
   name='Disecon',
-  version='0.1.9',
+  version='0.1.10',
   description='Disecon',
   long_description=readme,
   long_description_content_type="text/markdown",
