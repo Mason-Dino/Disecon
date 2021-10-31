46a2f4b2f5b33d6c8f72527d3991f992c0f9d8f5
Mason <masonnorberg@midco.net>
2021-10-31 14:19:21 -0500

    updated change log and version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ classifiers = [
  
 setup(
   name='Disecon',
-  version='0.5.1',
+  version='0.5.2',
   description='Disecon',
   long_description=readme,
   long_description_content_type="text/markdown",
