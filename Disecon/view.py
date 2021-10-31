970a5575132f00794e04deae9d129b8cbbd0f842
Mason <masonnorberg@midco.net>
2021-10-31 14:03:06 -0500

    removed lines

--- Disecon/view.py
+++ Disecon/view.py
@@ -7,74 +7,56 @@ class view:
     def __init__(self, user_ID=None):
         self.user_ID = user_ID
         
-    def wallet(self):
-        #user ID has none
-        if self.user_ID == None:
-            print("user_ID")
-        
-        #User ID is there
-        else:            
-            conn = sqlite3.connect("economy.db")
-            c = conn.cursor()
+    def wallet(self):                    
+        conn = sqlite3.connect("economy.db")
+        c = conn.cursor()
             
-            c.execute(f"SELECT * FROM economy WHERE user_ID={self.user_ID}")
+        c.execute(f"SELECT * FROM economy WHERE user_ID={self.user_ID}")
             
-            items = c.fetchall()
-            none = str(items)
+        items = c.fetchall()
+        none = str(items)
             
-            if none == "[]":
+        if none == "[]":
                 return "0"
             
-            else:
-                for item in items:
-                    wallet = item[1]
+        else:
+            for item in items:
+                wallet = item[1]
                     
-                return wallet        
+            return wallet        
 
-    def bank(self):
-        #user ID has none
-        if self.user_ID == None:
-            print("user_ID")
-        
-        #User ID is there
-        else:            
-            conn = sqlite3.connect("economy.db")
-            c = conn.cursor()
+    def wallet(self):                    
+        conn = sqlite3.connect("economy.db")
+        c = conn.cursor()
             
-            c.execute(f"SELECT * FROM economy WHERE user_ID={self.user_ID}")
+        c.execute(f"SELECT * FROM economy WHERE user_ID={self.user_ID}")
             
-            items = c.fetchall()
-            none = str(items)
+        items = c.fetchall()
+        none = str(items)
             
-            if none == "[]":
+        if none == "[]":
                 return "0"
             
-            else:
-                for item in items:
-                    bank = item[2]
+        else:
+            for item in items:
+                bank = item[2]
                     
-                return bank   
+            return bank   
         
-    def net(self):
-        #user ID has none
-        if self.user_ID == None:
-            print("user_ID")
-        
-        #User ID is there
-        else:            
-            conn = sqlite3.connect("economy.db")
-            c = conn.cursor()
+    def net(self):                    
+        conn = sqlite3.connect("economy.db")
+        c = conn.cursor()
             
-            c.execute(f"SELECT * FROM economy WHERE user_ID={self.user_ID}")
+        c.execute(f"SELECT * FROM economy WHERE user_ID={self.user_ID}")
             
-            items = c.fetchall()
-            none = str(items)
+        items = c.fetchall()
+        none = str(items)
             
-            if none == "[]":
+        if none == "[]":
                 return "0"
             
-            else:
-                for item in items:
-                    net = item[3]
+        else:
+            for item in items:
+                net = item[3]
                     
-                return net   
+            return net   
