b1ebaff1cbcb3fc38ca75d899b7564ca1ee409eb
LegosAndStuff-Dev <masonnorberg@midco.net>
2021-11-23 17:35:20 -0600

    remvoed Disecon class

--- Disecon/start.py
+++ Disecon/start.py
@@ -2,28 +2,24 @@
 
 import sqlite3
 
-class Disecon:
-    def __init__(self):
+def start():
+    conn = sqlite3.connect("economy.db")
+    c = conn.cursor()
+    
+    try:
+        c.execute("""CREATE TABLE economy (
+                user_ID int,
+                wallet int,
+                bank int,
+                net int
+                
+        )""")
+        
         pass
     
-    def start():
-        conn = sqlite3.connect("economy.db")
-        c = conn.cursor()
+    except:
+        print("table wrong")
         
-        try:
-            c.execute("""CREATE TABLE economy (
-                    user_ID int,
-                    wallet int,
-                    bank int,
-                    net int
-                    
-            )""")
-            
-            pass
-        
-        except:
-            print("table wrong")
-            
-        
-        conn.commit()
-        conn.close()
+    
+    conn.commit()
+    conn.close()
