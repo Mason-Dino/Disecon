2fc167abcf5de720db1cc4ad96e646cc50ae8d6d
LegosAndStuff-Dev <masonnorberg@midco.net>
2021-11-22 19:34:08 -0600

    testing top class to work

--- Disecon/top.py
+++ Disecon/top.py
@@ -2,66 +2,67 @@
 
 import sqlite3
 
-class top:
-    def __init__(self, place:int):
-        self.place = place
-        
-    def user_ID(self):
-        offset = self.place - 1
-        
-        conn =  sqlite3.connect("economy.db")
-        c = conn.cursor()
-        
-        c.execute(f"SELECT * FROM economy ORDER BY net DESC LIMIT 1 OFFSET {offset}")
-        
-        items = c.fetchall()
-        
-        for item in items:
-            user_ID = int(item[0])
+class Disecon:
+    class top:
+        def __init__(self, place:int):
+            self.place = place
             
-        return user_ID
+        def user_ID(self):
+            offset = self.place - 1
+            
+            conn =  sqlite3.connect("economy.db")
+            c = conn.cursor()
+            
+            c.execute(f"SELECT * FROM economy ORDER BY net DESC LIMIT 1 OFFSET {offset}")
+            
+            items = c.fetchall()
+            
+            for item in items:
+                user_ID = int(item[0])
+                
+            return user_ID
 
-    def wallet(self):
-        offset = self.place - 1
-        
-        conn =  sqlite3.connect("economy.db")
-        c = conn.cursor()
-        
-        c.execute(f"SELECT * FROM economy ORDER BY net DESC LIMIT 1 OFFSET {offset}")
-        
-        items = c.fetchall()
-        
-        for item in items:
-            wallet = int(item[1])
+        def wallet(self):
+            offset = self.place - 1
             
-        return wallet
-        
-    def bank(self):
-        offset = self.place - 1
-        
-        conn =  sqlite3.connect("economy.db")
-        c = conn.cursor()
-        
-        c.execute(f"SELECT * FROM economy ORDER BY net DESC LIMIT 1 OFFSET {offset}")
-        
-        items = c.fetchall()
-        
-        for item in items:
-            bank = int(item[2])
+            conn =  sqlite3.connect("economy.db")
+            c = conn.cursor()
             
-        return bank
-    
-    def net(self):
-        offset = self.place - 1
-        
-        conn =  sqlite3.connect("economy.db")
-        c = conn.cursor()
-        
-        c.execute(f"SELECT * FROM economy ORDER BY net DESC LIMIT 1 OFFSET {offset}")
-        
-        items = c.fetchall()
-        
-        for item in items:
-            net = int(item[3])
+            c.execute(f"SELECT * FROM economy ORDER BY net DESC LIMIT 1 OFFSET {offset}")
             
-        return net
+            items = c.fetchall()
+            
+            for item in items:
+                wallet = int(item[1])
+                
+            return wallet
+            
+        def bank(self):
+            offset = self.place - 1
+            
+            conn =  sqlite3.connect("economy.db")
+            c = conn.cursor()
+            
+            c.execute(f"SELECT * FROM economy ORDER BY net DESC LIMIT 1 OFFSET {offset}")
+            
+            items = c.fetchall()
+            
+            for item in items:
+                bank = int(item[2])
+                
+            return bank
+        
+        def net(self):
+            offset = self.place - 1
+            
+            conn =  sqlite3.connect("economy.db")
+            c = conn.cursor()
+            
+            c.execute(f"SELECT * FROM economy ORDER BY net DESC LIMIT 1 OFFSET {offset}")
+            
+            items = c.fetchall()
+            
+            for item in items:
+                net = int(item[3])
+                
+            return net
