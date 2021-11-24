2ead080cabb46c40e8463eb5f7503fbdb5e450e5
LegosAndStuff-Dev <masonnorberg@midco.net>
2021-11-23 21:17:02 -0600

    updated change log and version

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ classifiers = [
  
 setup(
   name='Disecon',
-  version='0.6.7',
+  version='0.6.8',
   description='Disecon',
   long_description=readme,
   long_description_content_type="text/markdown",
