99e3e24d9734da97ac15142f5c3715ec068ac535
LegosAndStuff-Dev <masonnorberg@midco.net>
2021-11-23 21:15:54 -0600

    removed imports

--- Disecon/__init__.py
+++ Disecon/__init__.py
@@ -1,8 +1,8 @@
-from .view import *
-from .wallet import *
-from .bank import *
+#from .view import *
+#from .wallet import *
+#from .bank import *
 from .start import *
-from .top import *
+#from .top import *
 
 from .money import *
 from .results import *
