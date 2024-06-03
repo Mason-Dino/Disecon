7785bc667e54ca24872b4baa97abf171a556e247
LegosAndStuff <masonnorberg@midco.net>
2024-06-03 07:26:41 -0500

    finnaly making it version 1.0.0

--- setup.py
+++ setup.py
@@ -5,16 +5,16 @@ here = pathlib.Path(__file__).parent.resolve()
 readme = (here / 'README.md').read_text(encoding='utf-8')
 
 classifiers = [
-  'Development Status :: 4 - Beta',
+  'Development Status :: 5 - Production/Stable Copy',
   'Intended Audience :: Developers',
   'Operating System :: OS Independent',
   'License :: OSI Approved :: MIT License',
   'Programming Language :: Python :: 3'
 ]
- 
+
 setup(
   name='Disecon',
-  version='0.6.10',
+  version='1.0.0',
   description='Disecon',
   long_description=readme,
   long_description_content_type="text/markdown",
